700a4c77d67f6644b98c86e4740206cece902e07
Yash Mathur <ym2977@nyu.edu>
2023-11-15 17:35:48 -0500

    Removed locks from retreive

--- parallel_spin.c
+++ parallel_spin.c
@@ -47,14 +47,11 @@ void insert(int key, int val) {
 // Returns NULL if the key isn't found in the table
 bucket_entry * retrieve(int key) {
   bucket_entry *b;
-  pthread_spin_lock(&spinlock);
   for (b = table[key % NUM_BUCKETS]; b != NULL; b = b->next) {
     if (b->key == key) {
-      pthread_spin_unlock(&spinlock);
       return b;
     }
   }
-  pthread_spin_unlock(&spinlock);
   return NULL;
 }
 
